75c735cde9b22f945e395d2796a4feef3bbdaa25
zx3282 <zx32829601@gmail.com>
2023-12-20 12:31:47 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <main class="home-page">
+    <h1>Home</h1>
+    <p>This is the home page</p>
+    
+  </main>
+</template>
---
+++ src/views/TeamView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <main class="team-page">
+    <h1>Team</h1>
+    <p>This is the team page</p>
+    
+  </main>
+</template>
